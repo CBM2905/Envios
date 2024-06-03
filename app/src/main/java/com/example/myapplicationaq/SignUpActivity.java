6273b91712df3b599d63d83d5068fcf7981b8ccd
yairh05 <133933607+yairh05@users.noreply.github.com>
2024-06-02 22:39:02 -0500

    Update SignUpActivity.java
    logica registro

--- app/src/main/java/com/example/myapplicationaq/SignUpActivity.java
+++ app/src/main/java/com/example/myapplicationaq/SignUpActivity.java
@@ -16,17 +16,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     EditText email;
     EditText password;
+    EditText name;
+    EditText lastName;
+    EditText address;
+    EditText user;
+    EditText passwordVerify;
+
+    private FirebaseFirestore db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent It = getIntent();
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
@@ -37,23 +50,54 @@ public class SignUpActivity extends AppCompatActivity implements PopupMenu.OnMen
         });
         email = findViewById(R.id.editTextTextEmailAddress);
         password = findViewById(R.id.editTextTextPassword);
+        name = findViewById(R.id.editTextPersonName);
+        lastName = findViewById(R.id.editTextPersonLastName);
+        address = findViewById(R.id.editTextAddress);
+        user = findViewById(R.id.editTextUser);
+        passwordVerify = findViewById(R.id.editTextPasswordVerify);
+
+        db = FirebaseFirestore.getInstance();
         FirebaseAuth auth = FirebaseAuth.getInstance();
+
+
         findViewById(R.id.buttonCreate).setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                auth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                if (!password.getText().toString().equals(passwordVerify.getText().toString())) {
+                    passwordVerify.setError("Las contraseñas deben ser iguales");
+                    return;
+                }
+
+                Map<String, Object> mp = new HashMap<>();
+                mp.put("Nombre", name.getText().toString());
+                mp.put("Apellido", lastName.getText().toString());
+                mp.put("Direccion", address.getText().toString());
+                mp.put("Usuario", user.getText().toString());
+                mp.put("Contraseña", password.getText().toString());
+                mp.put("Email", email.getText().toString());
+
+                db.collection("USUARIO").add(mp).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(getApplicationContext(),"Usuario creado",Toast.LENGTH_LONG);
-                        }
-                        else{
-                            Toast.makeText(getApplicationContext(),"usuario no creado",Toast.LENGTH_LONG);
-                        }
+                    public void onSuccess(DocumentReference documentReference) {
+                        auth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(getApplicationContext(),"Usuario creado",Toast.LENGTH_LONG).show();
+                                }
+                                else{
+                                    Toast.makeText(getApplicationContext(),"usuario no creado",Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
                     }
                 });
             }
         });
+
         findViewById(R.id.menuButton3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
